80e84e923a4d73078f4c1b6b45959035402dc818
MarcinSzymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2022-03-26 13:52:41 +0100

    Added dice images and image change functionality

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This app lets the user roll a dice with click on the roll button
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,21 +20,39 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.setOnClickListener {
             rollDice(dice)
-
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
-            // Alternatively:
-            // val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            // toast.show()
         }
 
+        // Displays a random dice roll when the app starts
+        rollDice(dice)
     }
 
+    /**
+     * Rolls a dice with the supplied Dice. The dice should be created somewhere else
+     * Display the result with the appropriate image
+     */
     private fun rollDice(d: Dice) {
+
         val rollRes = d.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = rollRes.toString()
+        val diceImg: ImageView = findViewById(R.id.imageView)
+
+        // Choose the appropriate image
+        val drawableResource = when(rollRes){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // What does setImageDrawable do?
+        diceImg.setImageResource(drawableResource)
+        diceImg.contentDescription = rollRes.toString()
     }
 
+    /**
+     * Create a dice with numSides sides
+     */
     class Dice(private val numSides: Int) {
 
         fun roll(): Int {
--- app/src/test/java/com/example/diceroller/DiceRollerTests.kt
+++ app/src/test/java/com/example/diceroller/DiceRollerTests.kt
@@ -1,8 +1,8 @@
 package com.example.diceroller
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,12 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun generates_number(){
+        val dice = MainActivity.Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
